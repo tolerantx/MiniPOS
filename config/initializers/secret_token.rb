b779f04483938fec4f8ea5f7469795bcb5f11d97
Abel Sierra <abel.sierra@agilekoding.com>
2013-07-04 20:13:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Alivio::Application.config.secret_key_base = '04d1b7b67627c4db53d7fc3f3c3600f1e07027cc4629cd0f9c9a7c8910f695dcbfc3b8134b5a24fc07224eb12f323bd53fba1ae045545ea982bb119e5a24ba56'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Alivio::Application.config.session_store :cookie_store, key: '_alivio_session'
