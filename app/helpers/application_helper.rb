6843596cc328d6599fb15794a8cc6e4954a22eb1
Abel Sierra <abel.sierra@agilekoding.com>
2013-07-12 17:52:56 -0500

    Added part of message notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  FLASH_NOTICE_KEYS = [:error, :notice, :warning, :alert, :success]
+
+  def flash_messages
+    return unless messages = flash.keys.select{|k| FLASH_NOTICE_KEYS.include?(k)}
+    formatted_messages = messages.map do |type|
+      content_tag :div, :class => "alert alert-#{type.to_s}" do
+        message_for_item(flash[type], flash["#{type}_item".to_sym])
+      end
+    end
+    flash.discard
+    raw formatted_messages.join
+  end
+
+  def message_for_item(message, item = nil)
+    msg = "#{link_to('x', '#', :class => 'close')}"
+    if item.is_a?(Array)
+      msg << content_tag(:p) do
+        message % can_link_to(*item)
+      end
+    else
+      msg << content_tag(:p) do
+        message % item
+      end
+    end
+    raw msg
+  end
 end
