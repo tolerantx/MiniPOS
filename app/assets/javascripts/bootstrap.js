c6c494eb56eeb8cbe47b661a4bc133bbe00462a1
Abel Sierra <abel.sierra@agilekoding.com>
2013-07-13 21:05:57 -0500

    Added automatic close alert message of bootstrap

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -2,4 +2,10 @@ jQuery(function() {
   $("a[rel=popover]").popover();
   $(".tooltip").tooltip();
   $("a[rel=tooltip]").tooltip();
+  createAutoClosingAlert(".alert", 3000);
+
+  function createAutoClosingAlert(selector, delay) {
+    var alert = $(selector).alert();
+    window.setTimeout(function() { alert.alert('close') }, delay);
+  }
 });
