116dea3ffb08e8bddc624f55bd8b457a7c476369
Abel Sierra <tolerantx@gmail.com>
2014-01-15 15:44:41 -0600

    Added reset password and action_mailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,17 @@ MiniPos::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: ENV["MINIPOS_HOST"] }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "mail.google.com",
+    authentication:       "login",
+    enable_starttls_auto: true,
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"]
+  }
 end
